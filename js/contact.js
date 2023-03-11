f9b9a9b8b2e639601bbc2b7a6fad7e77b9fa476f
MariuszRozycki <mariansc83@icloud.com>
2023-03-11 14:38:15 +0100

    Sening og data from contact form to API add.

--- js/contact.js
+++ js/contact.js
@@ -1,82 +1,86 @@
 const form = document.querySelector("#form");
-const userName = document.querySelector("#name");
+const userName = document.querySelector("#user-name");
 const nameError = document.querySelector("#name-error");
-const subject = document.querySelector("#subject");
+const subject = document.querySelector("#your-subject");
 const subjectError = document.querySelector("#subject-error");
-const email = document.querySelector("#email");
+const email = document.querySelector("#your-email");
 const emailError = document.querySelector("#email-error");
-const userMessage = document.querySelector("#user-message");
+const userMessage = document.querySelector("#your-message");
 const userMessageError = document.querySelector("#user-message--error");
 const message = document.querySelector("#message");
-const contactPageUrl = "https://mariuszrozycki.info/trip-blog/wp-json/wp/v2/pages?slug=contact";
-const h1Contact = document.querySelector("#h1-contact");
 
-async function getDataContactPage(url) {
+const formSubmissionHandler = (event) => {
+  event.preventDefault();
+
+  const form = event.target,
+    { action, method } = form,
+    body = new FormData(form);
+
   try {
-    const response = await fetch(url);
-    const results = await response.json();
+    fetch(action, {
+      method,
+      body
+    })
 
-    for (let result of results) {
-      h1Contact.innerHTML = `
-      ${result.title.rendered}
-      `;
+    function validateForm() {
+      if (checkLength(userName.value, 1)) {
+        nameError.style.display = "none";
+      } else {
+        nameError.style.display = "block";
+      }
+
+      if (checkLength(subject.value, 1)) {
+        subjectError.style.display = "none";
+      } else {
+        subjectError.style.display = "block";
+      }
+
+      if (checkEmail(email.value)) {
+        emailError.style.display = "none";
+      } else {
+        emailError.style.display = "block";
+      }
+
+      if (checkLength(userMessage.value, 1)) {
+        userMessageError.style.display = "none";
+      } else {
+        userMessageError.style.display = "block";
+      }
+
+      formCorrectlyValidated();
+    };
+
+    function checkLength(value, len) {
+      if (value.trim().length > len) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+
+    function checkEmail(email) {
+      const regEx = /\S+@\S+\.\S+/;
+      const patternMatches = regEx.test(email);
+      return patternMatches;
+    }
+
+    validateForm();
+
+    function formCorrectlyValidated() {
+      if (checkLength(userName.value, 1) && (checkLength(subject.value, 1)) && (checkEmail(email.value)) && (checkLength(userMessage.value, 1))) {
+        message.innerHTML = `<p class="success">Your message has been sent.</p>`;
+        form.reset();
+      }
     }
   }
+
   catch (error) {
-    console.log(error);
+    form.innerHTML = `An error occurred beyond your control! Call to <a href="tel:+4793928270">+47 939 28 270</a> (Mariusz Rozycki) to fix a problem.`;
   }
-}
-getDataContactPage(contactPageUrl);
-
-function validateForm(event) {
-  event.preventDefault();
-  if (checkLength(userName.value, 5)) {
-    nameError.style.display = "none";
-  } else {
-    nameError.style.display = "block";
-  }
-
-  if (checkLength(subject.value, 15)) {
-    subjectError.style.display = "none";
-  } else {
-    subjectError.style.display = "block";
-  }
-
-  if (checkEmail(email.value)) {
-    emailError.style.display = "none";
-  } else {
-    emailError.style.display = "block";
-  }
-
-  if (checkLength(userMessage.value, 25)) {
-    userMessageError.style.display = "none";
-  } else {
-    userMessageError.style.display = "block";
-  }
-
-  formCorrectlyValidated();
 };
 
-function checkLength(value, len) {
-  if (value.trim().length > len) {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
+form.addEventListener("submit", formSubmissionHandler);
 
-function checkEmail(email) {
-  const regEx = /\S+@\S+\.\S+/;
-  const patternMatches = regEx.test(email);
-  return patternMatches;
-}
 
-form.addEventListener("submit", validateForm);
 
-function formCorrectlyValidated() {
-  if (checkLength(userName.value, 5) && (checkLength(subject.value, 15)) && (checkEmail(email.value)) && (checkLength(userMessage.value, 25))) {
-    message.innerHTML = `<p class="success">Your message has been sent.</p>`;
-    form.reset();
-  }
-}
